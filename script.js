3debef623403b442d6500d40460700bbec7991f8
vpetrunek <v.petrunek@gmail.com>
2017-01-19 22:31:37 +0200

    Fixing

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 alert("Hello world!");
+var answer=0; 
+for (var i=0;i<5;i++){ 
+var x=Math.floor(Math.random()*100)+0; 
+var y=Math.floor(Math.random()*100)+0; 
+var z=x+y; 
+var d=prompt(x+"+"+y+"="); 
+d=parseInt(d); 
+if(Number.isNaN(d)) window.alert('Это ни число'); 
+if(d==z) answer++; 
+} 
+window.alert(answer); 
